97ac2b0b3e8e8968b835140f1f92868a2634a2db
Bayley <cbayley@proforma.com>
2018-04-02 16:14:07 -0400

    Added filter for currency * filled out the default data structure a bit more

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -1,9 +1,18 @@
 /* global angular */
 /* global stampit */
+
 'use strict';
 
 const geopardyApp = angular.module('geopardyApp', []);
 
+/* Add dollar sign to values */
+geopardyApp.filter('currency', () => {
+  return (amount) => {
+    return `\$${amount}`;
+  };
+});
+
+/* Cleaner way to pass Stampit around */
 geopardyApp.factory('Stampit', () => {
   const Stampit = stampit.default;
   return Stampit;
@@ -12,11 +21,15 @@ geopardyApp.factory('Stampit', () => {
 geopardyApp.controller('geopardyController', ['$scope', '$http', 'Stampit', ($scope, $http, Stampit) => {
   /* Configuration */
   const CATEGORY_COUNT = 5;
+
   $scope.values = [1, 2, 3, 4, 5].map(v => v * 200);
+  $scope.selectQuestion = (category, value) => {
+    console.log(category.getQuestion(value));
+  };
 
   /* Set up category data structures */
   $scope.categories = [];
-  for (let i = 0; i < 5; i++) {
+  for (let i = 0; i < CATEGORY_COUNT; i++) {
     /* Category stamp */
     const Category = Stampit({
       props: {
@@ -51,7 +64,11 @@ geopardyApp.controller('geopardyController', ['$scope', '$http', 'Stampit', ($sc
       }
     });
 
-    const category = Category({name: i});
+    const category = Category({ name: i });
+    for (let value of $scope.values) {
+      const question = Question({ value, text: 'Test question', answer: 'Test answer' });
+      category.setQuestion(question);
+    }
     $scope.categories.push(category);
   }
 }]);
