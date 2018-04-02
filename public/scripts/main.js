12e478fd13922229f79d6e1188cbb8e789d189b3
Bayley <cbayley@proforma.com>
2018-04-02 15:29:29 -0400

    Got a grid working and added some structure for where api data will go

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -2,7 +2,6 @@
 /* global stampit */
 'use strict';
 
-
 const geopardyApp = angular.module('geopardyApp', []);
 
 geopardyApp.factory('Stampit', () => {
@@ -11,5 +10,48 @@ geopardyApp.factory('Stampit', () => {
 });
 
 geopardyApp.controller('geopardyController', ['$scope', '$http', 'Stampit', ($scope, $http, Stampit) => {
-  console.log(Stampit);
+  /* Configuration */
+  const CATEGORY_COUNT = 5;
+  $scope.values = [1, 2, 3, 4, 5].map(v => v * 200);
+
+  /* Set up category data structures */
+  $scope.categories = [];
+  for (let i = 0; i < 5; i++) {
+    /* Category stamp */
+    const Category = Stampit({
+      props: {
+        name: null,
+        questions: {}
+      },
+      init({ name, questions = this.questions }) {
+        this.name = name;
+        this.questions = questions;
+      },
+      methods: {
+        setQuestion(question) {
+          this.questions[question.value] = question;
+        },
+        getQuestion(value) {
+          return this.questions[value] === undefined ? null : this.questions[value];
+        }
+      }
+    });
+
+    /* Question stamp */
+    const Question = Stampit({
+      props: {
+        value: null,
+        text: null,
+        answer: null
+      },
+      init({ value, text, answer }) {
+        this.value = value;
+        this.text = text;
+        this.answer = answer;
+      }
+    });
+
+    const category = Category({name: i});
+    $scope.categories.push(category);
+  }
 }]);
